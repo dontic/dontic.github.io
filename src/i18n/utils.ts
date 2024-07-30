2868bb94d2d78f83b9450ba91f72418ba84165d3
Daniel Garcia Sanchez <dontic@zoho.com>
2024-07-30 13:40:30 +0000

    refactor: Update Languages type class

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -2,7 +2,7 @@ import { ui, defaultLang } from "./ui";
 
 export { defaultLang };
 
-export type languages = keyof typeof ui;
+export type Languages = keyof typeof ui;
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
