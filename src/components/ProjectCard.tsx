f26a38621ff060c55706be090ee9069d5c2b221c
Daniel Garcia Sanchez <dontic@zoho.com>
2024-07-31 13:28:56 +0000

    chore: Add target="_blank" to project card link

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -39,6 +39,7 @@ export default function Card({
     <li className="my-6">
       <a
         href={href}
+        target="_blank"
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         <h3 {...headerProps}>{title}</h3>
