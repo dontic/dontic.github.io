9f9795288b84345244ff7b7cf35e718e88816e23
Daniel Garcia <github@mail.daniel.es>
2025-03-07 14:35:09 +0000

    Simplify dark mode configuration in Tailwind CSS to use a string format

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,5 @@ import typographyPlugin from '@tailwindcss/typography';
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
     plugins: [typographyPlugin],
-    darkMode: ['class'],
+    darkMode: 'class',
 };
