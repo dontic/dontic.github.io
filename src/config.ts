e86ec257d14e59d504687371477ee776da632606
Daniel Garcia <github@mail.daniel.es>
2025-03-07 12:44:27 +0000

    Add site configuration and logo SVG for branding

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+export const site = {
+    title: 'The Camel',
+    description: 'Personal Blog',
+    author: 'Daniel García',
+    siteUrl: 'https://daniel.es',
+    logo: 'thecamel-text'
+};
+
+export const navigation = {
+    header: {
+        links : [
+            { href: '/tech', text: 'Tech' },
+            { href: '/business', text: 'Business' },
+            { href: '/projects', text: 'Projects' },]
+    },
+    footer: {
+        title: 'My App',
+        description: 'My App Description',
+        keywords: 'my, app, keywords',
+    },
+};
+
+export const socialLinks = [
+    { name: 'X', url: 'https://x.com/onticdani' },
+    { name: 'GitHub', url: 'https://github.com/dontic' },
+]
