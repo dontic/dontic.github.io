2f55a71739fec061a6d8277198d078000b4d6837
Daniel Garcia <github@mail.daniel.es>
2025-03-10 10:58:40 +0000

    Install remark-toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
+import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  markdown: {
+    remarkPlugins: [[remarkToc, { heading: 'toc', maxDepth: 3 }]],
+  },
   integrations: [sitemap(), icon()],
 });
