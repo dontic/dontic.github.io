7d2caf05ec04d784b424c20fc3f9853eff65e3c5
Daniel Garcia <github@mail.daniel.es>
2025-08-06 11:47:06 +0000

    Fix noopener, noreferrer links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
-import { SITE } from './src/config';  // Use a relative import to avoid errors
-import pagefind from "astro-pagefind";
+import { SITE } from './src/config'; // Use a relative import to avoid errors
+import pagefind from 'astro-pagefind';
 import mdx from '@astrojs/mdx';
-
 import react from '@astrojs/react';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,6 +15,14 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [sitemap(), icon(), pagefind(), mdx(), react()],
+  integrations: [
+    sitemap(),
+    icon(),
+    pagefind(),
+    mdx({
+      rehypePlugins: [[rehypeExternalLinks, { target: '_blank', rel: [] }]],
+    }),
+    react(),
+  ],
   trailingSlash: 'always',
 });
